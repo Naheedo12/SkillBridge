134dc0db0d8a3f1d3b646501e7ff811368a63fcd
Naheedo12 <elqadisalma1@gmail.com>
2025-12-14 23:45:48 +0100

    Preparation de l'Environement de developpement : React (Vite, TailwindCSS) + Laravel

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        SkillBridge – React + Tailwind 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
